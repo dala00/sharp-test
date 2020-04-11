68b1f00f74a8d576c2d9201776c5ad9624669e4d
Shoichi Namba <alphabrend@gmail.com>
2020-04-11 22:01:00 +0900

    Aiueo

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const path = require('path')
 const sharp = require('sharp')
 
 module.exports = async (_req, res) => {
-  const image = fs.readFileSync(path.join(__dirname, 'image.png'))
-  const resizedImage = await sharp(image).resize(100).toBuffer()
-  res.end(resizedImage, 'binary')
+  res.end('aiueo')
 }
