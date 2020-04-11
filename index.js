d1d75979bcc9b1d6cab050ed0752b5d22d6593e2
Shoichi Namba <alphabrend@gmail.com>
2020-04-11 19:54:01 +0900

    Resize image

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const fs = require('fs')
+const sharp = require('sharp')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  const image = fs.readFileSync('image.png')
+  const resizedImage = await sharp(image).resize(100).toBuffer()
+  res.end(resizedImage, 'binary')
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
