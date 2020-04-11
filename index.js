a4e1a0b8f363ea7060fe15e4e264578116c3c5e9
Shoichi Namba <alphabrend@gmail.com>
2020-04-11 20:22:54 +0900

    Set port

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ app.get('/', async (req, res) => {
   res.end(resizedImage, 'binary')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Example app listening on port 3000!')
+)
